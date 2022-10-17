4606f42f2d6b2939a0673542545af55dc32fc253
MiguelSMaciel <miguel.santosmaciel@gmail.com>
2022-10-17 12:27:44 -0300

    anim bear

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -8,18 +8,32 @@ public class EnemyController : MonoBehaviour
     private GameObject Heroi;
     private NavMeshAgent Agente;
     Stats statsScript;
+
+    public GameObject particleSpawn;
+    public Animator anim;
+
+    private float tempoAnimSpawn=1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
         statsScript = GetComponent<Stats>();
         Agente = GetComponent<NavMeshAgent>();
         Heroi = GameObject.FindGameObjectWithTag("Player");
+        anim.SetBool("walk", false);
+        particleSpawn.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
+        tempoAnimSpawn -= Time.deltaTime;
+        if(tempoAnimSpawn < 0)
+        {
         Agente.SetDestination(Heroi.transform.position);
+            anim.SetBool("walk", true);
+            particleSpawn.SetActive(false);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -27,6 +41,7 @@ public class EnemyController : MonoBehaviour
         if (other.gameObject.tag == "AreaAtkHeroi")
         {
             statsScript.health -= 40;
+            anim.SetBool("Die", true);
         }
     }
 }
